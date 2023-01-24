905777490d2275e3809008e481380c0034ddb553
wjdgh4058 <ljhmoa@gmail.com>
2023-01-24 16:58:23 +0900

    [추가] mousemove 색상변환 학습

--- main.js
+++ main.js
@@ -5,6 +5,31 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
+ctx.lineWidth = 2;
+
+const colors = [
+  "#ff3838",
+  "#ffb8b8",
+  "#c56cf0",
+  "#ff9f1a",
+  "#fff200",
+  "#32ff7e",
+  "#7efff5",
+  "#18dcff",
+  "#7d5fff",
+];
+
+function onclick(e) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onclick);
+
 /* ctx.fillRect(200, 200, 50, 200);
 ctx.fillRect(400, 200, 50, 200);
 ctx.lineWidth = 2;
@@ -17,7 +42,7 @@ ctx.lineTo(325, 100);
 ctx.lineTo(450, 200);
 ctx.fill(); */
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
+/* ctx.fillRect(210 - 40, 200 - 30, 15, 100);
 ctx.fillRect(350 - 40, 200 - 30, 15, 100);
 ctx.fillRect(260 - 40, 200 - 30, 60, 200);
 
@@ -27,6 +52,6 @@ ctx.fill();
 ctx.beginPath();
 ctx.fillStyle = "white";
 
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+ctx.arc(260 + 10, 80, 8, 0, 1 * Math.PI);
+ctx.arc(220 + 10, 80, 8, 0, 1 * Math.PI);
+ctx.fill(); */
